1cd8847584314c22d09ead71f621878a1966a24b
ourongxing <ourongxing>
2021-01-17 13:57:00 +0800

    mrdkkey 改为自动生成，resolved #1

---
+++ mrdkkey.js
@@ -0,0 +1,65 @@
+const dateCode = [
+  "s9ZS",
+  "jQkB",
+  "RuQM",
+  "O0_L",
+  "Buxf",
+  "LepV",
+  "Ec6w",
+  "zPLD",
+  "eZry",
+  "QjBF",
+  "XPB0",
+  "zlTr",
+  "YDr2",
+  "Mfdu",
+  "HSoi",
+  "frhT",
+  "GOdB",
+  "AEN0",
+  "zX0T",
+  "wJg1",
+  "fCmn",
+  "SM3z",
+  "2U5I",
+  "LI3u",
+  "3rAY",
+  "aoa4",
+  "Jf9u",
+  "M69T",
+  "XCea",
+  "63gc",
+  "6_Kf"
+];
+const hourCode = [
+  "89KC",
+  "pzTS",
+  "wgte",
+  "29_3",
+  "GpdG",
+  "FDYl",
+  "vsE9",
+  "SPJk",
+  "_buC",
+  "GPHN",
+  "OKax",
+  "_Kk4",
+  "hYxa",
+  "1BC5",
+  "oBk_",
+  "JgUW",
+  "0CPR",
+  "jlEh",
+  "gBGg",
+  "frS6",
+  "4ads",
+  "Iwfk",
+  "TCgR",
+  "wbjP"
+];
+
+module.exports = {
+  getMrdkKey(d, h) {
+    return dateCode[d] + hourCode[h];
+  }
+};
