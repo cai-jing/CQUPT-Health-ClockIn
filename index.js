bc3540f4b37c6d6174cdb999cd79c25c1635548f
cai-jing <52103274+cai-jing@users.noreply.github.com>
2021-02-02 22:51:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,55 @@
-// version v0.1
+// version v1.0
 // create by ourongxing
 // detail url: https://github.com/ourongxing/CQUPT-Health-ClockIn
 
-const ax = require("axios");
 const { getMrdkKey } = require("./mrdkkey.js");
+const ax = require("axios");
 
-// Server酱推送 KEY
-const push_key = process.env.PUSH_KEY;
+// 获取北京时间
+const getLocalTime = () => new Date(Date.now() + 8 * 60 * 60 * 1000);
+// 获取格式化后时间
+const getFormatTime = () =>
+  `${dateFormat(getLocalTime(), "%H:%M:%S", true)} | `;
+// 获取当前时间戳
+const getTimeStamp = () => Math.floor(Date.now() / 1000);
 
 // 私密信息，通过 Github secrets 填入
 const secret_keys = {
+  push_key: process.env.PUSH_KEY,
+  push_key_qq: process.env.PUSH_KEY_QQ,
   openid: process.env.OPEN_ID,
   student_num: process.env.STUDENT_NUM,
   address: process.env.ADDRESS
 };
 
+// 修改默认的 console.log() 函数，集成多个功能
+let logs = "";
+console.oldLog = console.log;
+console.log = function(str) {
+  console.oldLog(getFormatTime() + str);
+  logs += getFormatTime() + str + "  \n";
+  if (str.indexOf("自动打卡失败") != -1) {
+    logs +=
+      "\n打卡失败的原因还可能有  \n" +
+      "1、填写的信息出错  \n" +
+      "2、地址简写或过于偏僻  \n" +
+      "3、如确定未出现以上已知的问题，请前往 Github 提 Issue";
+    sendNotification("自动打卡失败，点击查看详情");
+  }
+  if (str.indexOf("自动打卡成功") != -1) {
+    sendNotification("自动打卡成功");
+  }
+  if (str.indexOf("检测重复打卡失败") != -1) {
+    sendNotification("检测重复打卡失败");
+  }
+};
+
 // 超时重试
 ax.interceptors.response.use(undefined, function axiosRetryInterceptor(err) {
   const config = err.config;
-  if(!config || !config.retry) return Promise.reject(err);
+  if (!config || !config.retry) return Promise.reject(err);
   config.__retryCount = config.__retryCount || 0;
-  if(config.__retryCount >= config.retry) {
+  if (config.__retryCount >= config.retry) {
     return Promise.reject(err);
   }
   config.__retryCount += 1;
@@ -32,14 +61,23 @@ ax.interceptors.response.use(undefined, function axiosRetryInterceptor(err) {
   return backoff.then(() => ax(config));
 });
 
-// 获取当前时间戳
-const getTimeStamp = () => Math.floor(Date.now() / 1000);
+//默认超时时间
+ax.defaults.timeout = 15000;
+//默认重试次数
+ax.defaults.retry = 5;
+//默认间隔时间
+ax.defaults.retryDelay = 1000;
 
 // 检查重复打卡
 function checkRepeatClock() {
+  if (getLocalTime().getUTCHours() > 0 && getLocalTime().getUTCHours() < 6) {
+    console.log("1 到 6 点查询学生信息的接口关闭");
+    console.log("自动打卡失败");
+    return;
+  }
   if (!secret_keys.student_num) {
-    console.log("1、没有填写学号");
-    console.log("2、打卡失败");
+    console.log("没有填写学号");
+    console.log("自动打卡失败");
     return;
   }
   const key = {
@@ -52,45 +90,37 @@ function checkRepeatClock() {
     method: "POST",
     data: {
       key: key_base64
-    },
-    retry: 5, // 重试次数
-    timeout: 10000, // 超时时间
-    retryDelay: 1000 // 间隔时间
+    }
   };
   ax(options)
     .then(res => {
       if (res.data.status == 200) {
         let count = res.data.data.count;
         if (count == "0") {
-          console.log("1、检测重复打卡-今日首次打卡");
+          console.log("检测重复打卡-今日首次打卡");
           getStudentInfo();
         } else {
-          console.log("1、检测重复打卡-今日已打卡");
-          console.log("2、打卡成功");
-          return;
+          console.log("检测重复打卡-今日已打卡");
+          console.log("自动打卡成功");
         }
       } else {
-        console.log("1、检测重复打卡失败");
-        return;
+        console.log("检测重复打卡失败");
       }
     })
     .catch(() => {
-      console.log("1、检测重复打卡失败");
-      return;
+      console.log("检测重复打卡失败");
     });
 }
 
 // 获取学生信息
 function getStudentInfo() {
   const options = {
-    url: "https://cyxbsmobile.redrock.team/cyxbsMobile/index.php/home/searchPeople/peopleList",
+    url:
+      "https://cyxbsmobile.redrock.team/cyxbsMobile/index.php/home/searchPeople/peopleList",
     method: "GET",
     params: {
       stu: secret_keys.student_num
-    },
-    retry: 5,
-    timeout: 10000,
-    retryDelay: 1000
+    }
   };
   ax(options)
     .then(res => {
@@ -98,28 +128,24 @@ function getStudentInfo() {
         let data = res.data.data[0];
         secret_keys.name = data.name;
         secret_keys.sex = data.gender;
-        console.log("2、获取学生信息成功");
+        console.log("获取学生信息成功");
         getLocation();
       } else {
-        console.log("2、获取学生信息失败");
-        console.log("3、打卡失败");
-        sendNotification("自动健康打卡失败，请手动打卡");
-        return;
+        console.log("获取学生信息失败");
+        console.log("自动打卡失败");
       }
     })
-    .catch(err => {
-      console.log("2、获取学生信息失败");
-      console.log("3、打卡失败");
-      sendNotification("自动健康打卡失败，请手动打卡");
-      return;
+    .catch(() => {
+      console.log("获取学生信息失败");
+      console.log("自动打卡失败");
     });
 }
 
 // 获取位置信息
 function getLocation() {
   if (!secret_keys.address) {
-    console.log("3、没有填写地址");
-    console.log("4、打卡失败");
+    console.log("没有填写地址");
+    console.log("自动打卡失败");
     return;
   }
   const options = {
@@ -128,10 +154,7 @@ function getLocation() {
     params: {
       address: secret_keys.address,
       key: "PULBZ-BSEWU-MAEVV-2IAJR-ZCAS3-53F4O"
-    },
-    retry: 5,
-    timeout: 10000,
-    retryDelay: 1000
+    }
   };
   ax(options)
     .then(res => {
@@ -145,35 +168,30 @@ function getLocation() {
           result.address_components.district +
           result.title;
         secret_keys.locationBig = `中国,${result.address_components.province},${result.address_components.city},${result.address_components.district}`;
-        console.log("3、获取地址成功");
+        console.log("获取地址成功");
         clockIn();
       } else {
-        console.log("3、获取地址失败");
-        console.log("4、打卡失败");
-        sendNotification("自动健康打卡失败，请手动打卡");
-        return;
+        console.log("获取地址失败");
+        console.log("自动打卡失败");
       }
     })
     .catch(() => {
-      console.log("3、获取地址失败");
-      console.log("4、打卡失败");
-      sendNotification("自动健康打卡失败，请手动打卡");
-      return;
+      console.log("获取地址失败");
+      console.log("自动打卡失败");
     });
 }
 
 // 打卡
 function clockIn() {
   if (!secret_keys.openid) {
-    console.log("4、没有填写 OPEN_ID");
-    console.log("5、打卡失败");
+    console.log("没有填写 OPEN_ID");
+    console.log("自动打卡失败");
     return;
   }
-  const time = new Date();
-
   //生成从10到99的随机数，用于每天小幅改变经纬度
-  const random = (min,max) => { return parseInt(Math.random()*(max-min+1)+min,10)};
-
+  const random = (min, max) => {
+    return parseInt(Math.random() * (max - min + 1) + min, 10);
+  };
   const key = {
     openid: secret_keys.openid.replace(/[\r\n]/g, ""),
     name: secret_keys.name,
@@ -181,8 +199,8 @@ function clockIn() {
     xb: secret_keys.sex,
     locationBig: secret_keys.locationBig,
     locationSmall: secret_keys.locationSmall,
-    latitude: secret_keys.lat+random(10,99)*0.000001,
-    longitude: secret_keys.lng+random(10,99)*0.000001,
+    latitude: secret_keys.lat + random(10, 99) * 0.000001,
+    longitude: secret_keys.lng + random(10, 99) * 0.000001,
     szdq: secret_keys.addressBig,
     xxdz: secret_keys.address,
 
@@ -200,73 +218,117 @@ function clockIn() {
     beizhu: "无",
     // 当前时间戳
     timestamp: getTimeStamp(),
-    mrdkkey: getMrdkKey(time.getDate(), time.getHours())
+    mrdkkey: getMrdkKey(
+      getLocalTime().getUTCDate(),
+      getLocalTime().getUTCHours()
+    )
   };
-
   const key_base64 = new Buffer.from(JSON.stringify(key)).toString("base64");
-
   const options = {
     url: "https://we.cqu.pt/api/mrdk/post_mrdk_info.php",
     method: "POST",
     headers: {
-      "User-Agent": "Mozilla/5.0 (Linux; Android 10; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.62 XWEB/2693 MMWEBSDK/201001 Mobile Safari/537.36 MMWEBID/7311 MicroMessenger/7.0.20.1781(0x27001439) Process/appbrand2 WeChat/arm64 NetType/4G Language/zh_CN ABI/arm64",
+      "User-Agent":
+        "Mozilla/5.0 (Linux; Android 10; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.62 XWEB/2693 MMWEBSDK/201001 Mobile Safari/537.36 MMWEBID/7311 MicroMessenger/7.0.20.1781(0x27001439) Process/appbrand2 WeChat/arm64 NetType/4G Language/zh_CN ABI/arm64",
       Referer: "https://servicewechat.com/wx8227f55dc4490f45/76/page-frame.html"
     },
     data: {
       key: key_base64
-    },
-    retry: 5,
-    timeout: 10000,
-    retryDelay: 1000
+    }
   };
-
   ax(options)
     .then(res => {
       if (res.data.status == 200) {
-        console.log("4、打卡成功");
-        sendNotification("自动健康打卡成功");
+        console.log("自动打卡成功");
       } else {
-        console.log("4、打卡失败");
-        sendNotification("自动健康打卡失败，请手动打卡");
+        console.log("自动打卡失败");
       }
     })
     .catch(() => {
-      console.log("4、打卡失败");
-      sendNotification("自动健康打卡失败，请手动打卡");
+      console.log("自动打卡失败");
     });
 }
 
-// 调用 Server 酱发送打卡信息到微信
+// 推送消息，填写哪一个的 PUSH_KEY 就使用哪一个，如果都填写则默认使用 Qmsg 酱推送到 QQ
 function sendNotification(text) {
-  if (!push_key) {
+  if (secret_keys.push_key_qq) {
+    const push_key_qq = secret_keys.push_key_qq.replace(/[\r\n]/g, "");
+    const options = {
+      url: "https://qmsg.zendee.cn/send/" + push_key_qq,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      data: {
+        msg: "自动健康打卡\n\n" + logs
+      },
+      transformRequest: [data => `msg=${data.msg}`]
+    };
+    ax(options)
+      .then(res => {
+        if (res.data.success) {
+          console.log("发送通知成功");
+        } else {
+          console.log("发送通知失败");
+        }
+      })
+      .catch(() => {
+        console.log("发送通知失败");
+      });
+  } else if (secret_keys.push_key) {
+    const push_key = secret_keys.push_key.replace(/[\r\n]/g, "");
+    const options = {
+      url: `https://sc.ftqq.com/${push_key}.send`,
+      method: "GET",
+      params: {
+        text: text,
+        desp: logs
+      }
+    };
+    ax(options)
+      .then(res => {
+        const code = res.data.errno;
+        if (code == 0) {
+          console.log("发送通知成功");
+        } else {
+          console.log("发送通知失败：" + res.data.errmsg);
+        }
+      })
+      .catch(() => {
+        console.log("发送通知失败");
+      });
+  } else {
     return;
   }
+}
 
-  let SCKEY = push_key.replace(/[\r\n]/g, "");
-
-  const options = {
-    url: `https://sc.ftqq.com/${SCKEY}.send`,
-    method: "GET",
-    params: {
-      text: text
-    },
-    retry: 5,
-    timeout: 10000,
-    retryDelay: 1000
-  };
-
-  ax(options)
-    .then(res => {
-      const code = res.data.errno;
-      if (code == 0) {
-        console.log("5、发送通知成功");
-      } else {
-        console.log("5、发送通知失败：" + res.data.errmsg);
-      }
-    })
-    .catch(() => {
-      console.log("5、发送通知失败");
-    });
+// 时间格式化
+function dateFormat(date, fstr, utc) {
+  utc = utc ? "getUTC" : "get";
+  return fstr.replace(/%[YmdHMS]/g, function(m) {
+    switch (m) {
+      case "%Y":
+        return date[utc + "FullYear"]();
+      case "%m":
+        m = 1 + date[utc + "Month"]();
+        break;
+      case "%d":
+        m = date[utc + "Date"]();
+        break;
+      case "%H":
+        m = date[utc + "Hours"]();
+        break;
+      case "%M":
+        m = date[utc + "Minutes"]();
+        break;
+      case "%S":
+        m = date[utc + "Seconds"]();
+        break;
+      default:
+        return m.slice(1);
+    }
+    return ("0" + m).slice(-2);
+  });
 }
 
 checkRepeatClock();
